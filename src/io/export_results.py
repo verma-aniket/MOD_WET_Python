d840b120886ca7e8b92a57af9d2c3260ec27ef4d
verma-aniket <aniketv@stanford.edu>
2026-08-20 03:04:36 -0400

    Minor datetime fix when exporting results

--- src/io/export_results.py
+++ src/io/export_results.py
@@ -13,14 +13,16 @@ def save_simulation_results_netcdf(model, output_path: str | Path) -> None:
     end_day = model.control.start_day + model.control.n_days
     time_states_val = np.linspace(model.control.start_day, end_day, model.control.nt + 1, dtype=np.float32)
     time_fluxes_val = np.linspace(model.control.start_day, end_day, model.control.nt, dtype=np.float32)
-    time_maps_val = np.linspace(model.control.start_day, end_day, model.map_outputs.n_map, dtype=np.float32)
+    # time_maps_val = np.linspace(model.control.start_day, end_day, model.map_outputs.n_map, dtype=np.float32)
 
     # Convert time coordinates to time stamps
     first_date_string = model.forcing.start_date_time
     first_date = np.datetime64(first_date_string)
     time_states = first_date + (time_states_val * 86400).astype("timedelta64[s]")
     time_fluxes = first_date + (time_fluxes_val * 86400).astype("timedelta64[s]")
-    time_maps = first_date + (time_maps_val * 86400).astype("timedelta64[s]") 
+    # time_maps = first_date + (time_maps_val * 86400).astype("timedelta64[s]") 
+    time_maps = first_date + (np.arange(0,model.map_outputs.n_map)).astype("timedelta64[D]")
+    print(time_maps)
 
     # 2. Extract Spatial Coordinates
     northing = (
